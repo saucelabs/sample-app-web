1f8bdb289d3f330ad22b2b3912af7f7b5d1f1618
Ryan Slobojan <ryan.slobojan@saucelabs.com>
2018-10-05 09:19:15 -0700

    Initial development environment and first page created
    * Setup, build and run steps documented in README
    * Vagrant-based development environment on Ubuntu
      18.04LTS using NodeJS 10
    * Added initial login page which approximately follows
      the style in the mockup - final skinning still TBD
    * Implemented login page using React and JSX
    * Babel set up to transcode from JSX to JS
    * Added dummy inventory page for post-login
    * TBD: login validation, any non-blank string
      works right now
    * TBD: list of valid logins on the login page

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-# sample-app-web
+# What is this codebase?
 This is the Sauce Labs Sample Application which is designed to be used from desktop web browsers
 
-# Sauce?
+# Setup
 
-Yes, Sauce.
+To set up the development environment for this sample app:
+
+1. Install [Vagrant](https://www.vagrantup.com/)
+2. Install [VirtualBox](https://www.virtualbox.org/)
+3. Run the `setup-env.sh` script in the root of the repository
+4. There is no step 4, you're done!
+
+# Build
+
+To build the application:
+
+1. `vagrant ssh` inside the project folder
+2. On the VM: `cd /usr/local/saucelabs/sample-app-web`
+3. On the VM: `./build-app.sh`
+4. Again, no step 4. The build is complete!
+
+# Run
+
+To run the built application (which is a static website):
+
+1. Open _[project-root]_`/bin/index.html` in a web browser
+2. Click around - this is the app!
---
+++ src/js/login-controls.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const e = React.createElement;
+
+class LoginButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+  	  username: '',
+	  password: '',
+	  error: ''
+    };
+
+    this.handlePassChange = this.handlePassChange.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.dismissError = this.dismissError.bind(this);
+  }
+
+  dismissError() {
+    this.setState({ error: '' });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    if (!this.state.username) {
+      return this.setState({ error: 'Username is required' });
+    }
+
+    if (!this.state.password) {
+      return this.setState({ error: 'Password is required' });
+    }
+
+    // If we're here, we have a username and password. Redirect!
+    window.location.href = './inventory.html';
+    return '';
+  }
+
+  handleUserChange(evt) {
+    this.setState({
+      username: evt.target.value,
+    });
+  };
+
+  handlePassChange(evt) {
+    this.setState({
+      password: evt.target.value,
+    });
+  }
+
+  
+  render() {
+
+	  return (
+		<div className="login-box">
+		  <form onSubmit={this.handleSubmit}>
+			  <input type="text" className="login-input" data-test="username" placeholder="Username" value={this.state.username} onChange={this.handleUserChange} />			  
+			  <input type="password" className="login-input" data-test="password" placeholder="Password" value={this.state.password} onChange={this.handlePassChange} />			  
+			  <input type="submit" className="login-button" value="LOGIN" />
+			  {
+					  this.state.error &&
+					  <h3 data-test="error" onClick={this.dismissError}>
+					  <button onClick={this.dismissError}>✖</button>
+					  Epic sadface: {this.state.error}
+					  </h3>
+			  }
+		  </form>
+		</div>
+	  );
+  }
+}
+
+const domContainer = document.querySelector('#login_button_container');
+ReactDOM.render(e(LoginButton), domContainer);
