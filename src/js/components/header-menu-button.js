9ce4ddc37f4e5e876428a02ee979e843e91c7f6c
Ryan Slobojan <ryan.slobojan@saucelabs.com>
2018-10-12 08:43:35 -0700

    Added a broken link for problem_user in the sidebar menu
    * 'About' link becomes broken for this user, leads to 404 page

--- src/js/components/header-menu-button.js
+++ src/js/components/header-menu-button.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { slide as Menu } from 'react-burger-menu'
+import { Credentials } from './credentials.js';
 
 class MenuButton extends Component {
   constructor(props) {
@@ -11,7 +12,7 @@ class MenuButton extends Component {
     return (
       <Menu pageWrapId={ "contents_wrapper" } outerContainerId={ "page_wrapper" }>
         <a id="home" className="menu-item" href="#">Home</a>
-        <a id="about" className="menu-item" href="https://saucelabs.com/">About</a>
+        <a id="about" className="menu-item" href={ Credentials.isProblemUser() ? "https://saucelabs.com/error/404" : "https://saucelabs.com/" }>About</a>
         <a id="contact" className="menu-item" href="./index.html">Logout</a>
         <a className="menu-item--small" href="">Settings</a>
       </Menu>
