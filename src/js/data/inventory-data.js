9cafa916e8c0b3bebb5416fec056d406e5f46da8
Ryan Slobojan <ryan.slobojan@saucelabs.com>
2018-10-22 20:39:44 -0700

    Added updated titles and descriptions to inventory data

--- src/js/data/inventory-data.js
+++ src/js/data/inventory-data.js
@@ -2,47 +2,39 @@ export class InventoryData {}
 
 InventoryData.ITEMS = [
   {
-    name: 'Frank\'s RedHot Original Cayenne Pepper Sauce',
-    desc: `The perfect blend of flavor and heat, Frank's cayenne pepper sauce features a delectable mixture of aged cayenne pepper.
-           This cayenne pepper hot sauce was the secret ingredient used in the original Buffalo wings created in Buffalo, NY in 1964`,
+    name: 'Sauce Labs Bike Light',
+    desc: `A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.`,
     price: '$9.99',
     image_url: './img/bike-light-1200x1500.jpg'
   },
   {
-    name: 'Sriracha Sauce',
-    desc: `Sriracha is a type of hot sauce or chili sauce made from a paste of chili peppers, distilled vinegar, garlic, sugar, and salt.
-           It is named after the coastal city of Si Racha, in Chonburi Province of eastern Thailand, where it may have been first produced
-           for dishes served at local seafood restaurants.`,
-    price: '$13.37',
+    name: 'Sauce Labs Bolt T-Shirt',
+    desc: `Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.`,
+    price: '$15.99',
     image_url: './img/bolt-shirt-1200x1500.jpg'
   },
   {
-    name: 'Cholula Hot Sauce Variety Pack - 5 Different Flavors',
-    desc: `This variety pack contains Five bottles of Cholula Hot Sauce in Five Different Flavors.
-           Flavors are one each of: Original, Chili-Lime, Chili-Garlic, Chipotle, and Green Pepper.
-           Five 5-Ounce bottles with distinctive Cholula artwork and carved wooden cap.
-           Great way to try all the delicious Cholula flavors!`,
-    price: '$313.37',
+    name: 'Sauce Labs Onesie',
+    desc: `Rib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.`,
+    price: '$7.99',
     image_url: './img/red-onesie-1200x1500.jpg'
   },
   {
-    name: 'Italian Hemp and Basil Pesto Sauce',
-    desc: `I hope you're hungry, because this delicious hemp and basil pesto will really jumpstart your appetite!
-           WARNING: Not to be consumed before participating in eating contests`,
-    price: '$4.20',
+    name: 'Test.allTheThings() T-Shirt (Red)',
+    desc: `This classic Sauce Labs t-shirt is perfect to wear when cozying up to your keyboard to automate a few tests. Super-soft and comfy ringspun combed cotton.`,
+    price: '$15.99',
     image_url: './img/red-tatt-1200x1500.jpg'
   },
   {
-    name: 'Prego Pizza Sauce, Pizzeria Style, 14 Ounce (Pack of 12)',
-    desc: `Make the pizza flavors you love at home!
-           12-pack is suitable for large gatherings, or for individual use after consuming Italian Hemp and Basil Pesto Sauce`,
-    price: '$39.99',
+    name: 'Sauce Labs Backpack',
+    desc: `carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.`,
+    price: '$29.99',
     image_url: './img/sauce-backpack-1200x1500.jpg'
   },
   {
-    name: 'Awesome Sauce',
-    desc: `You'll definitely want to test this sauce out for yourself! Goes great with pizzas, pastas, and iPhones.`,
-    price: '$20.08',
+    name: 'Sauce Labs Fleece Jacket',
+    desc: `It's not every day that you come across a midweight quarter-zip fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office.`,
+    price: '$49.99',
     image_url: './img/sauce-pullover-1200x1500.jpg'
   }
 ]
