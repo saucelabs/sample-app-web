4617eef72782e280e61cdbd146c8f3009acda557
Daniel Kopka <daniel@saucelabs.com>
2020-07-15 09:50:48 +0200

    Add POC tracking to saucedemo

--- src/index.js
+++ src/index.js
@@ -6,3 +6,12 @@ import InventoryItem from "./js/components/inventory-item.js"
 import CartContents from "./js/components/cart-contents.js"
 import CheckoutInfo from "./js/components/checkout-info.js"
 import CheckoutSummary from "./js/components/checkout-summary.js"
+import * as backtrace from 'backtrace-js'
+
+
+if (process.env.BACKTRACE_TOKEN) {
+  backtrace.initialize({
+    endpoint: `https://submit.backtrace.io/saucelabs/${process.env.BACKTRACE_TOKEN}/json`,
+    token: process.env.BACKTRACE_TOKEN,
+  });
+}
