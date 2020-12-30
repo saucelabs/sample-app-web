1363bce95cf8cf4a66411698dd5ea739957eaf7e
Wim Selles <wim@wswebcreation.nl>
2020-12-30 15:59:16 +0100

    test: disable IE (for now)

--- new/test/e2e/configs/wdio.saucelabs.conf.js
+++ new/test/e2e/configs/wdio.saucelabs.conf.js
@@ -25,25 +25,25 @@ config.baseUrl = 'http://localhost:5000';
 // Capabilities
 // ============
 config.capabilities = [
-    // /**
-    //  * Desktop browsers
-    //  */
-    // {
-    //     browserName: 'chrome',
-    //     platformName: 'Windows 10',
-    //     browserVersion: 'latest',
-    //     'sauce:options': {
-    //         ...defaultBrowserSauceOptions,
-    //     },
-    // },
-    // {
-    //     browserName: 'firefox',
-    //     platformName: 'Windows 10',
-    //     browserVersion: 'latest',
-    //     'sauce:options': {
-    //         ...defaultBrowserSauceOptions,
-    //     },
-    // },
+    /**
+     * Desktop browsers
+     */
+    {
+        browserName: 'chrome',
+        platformName: 'Windows 10',
+        browserVersion: 'latest',
+        'sauce:options': {
+            ...defaultBrowserSauceOptions,
+        },
+    },
+    {
+        browserName: 'firefox',
+        platformName: 'Windows 10',
+        browserVersion: 'latest',
+        'sauce:options': {
+            ...defaultBrowserSauceOptions,
+        },
+    },
     {
         browserName: 'internet explorer',
         platformName: 'Windows 8.1',
@@ -53,48 +53,48 @@ config.capabilities = [
             iedriverVersion: '3.141.59',
         },
     },
-    // {
-    //     browserName: 'MicrosoftEdge',
-    //     platformName: 'Windows 10',
-    //     browserVersion: '18.17763',
-    //     'sauce:options': {
-    //         ...defaultBrowserSauceOptions,
-    //     },
-    // },
-    // {
-    //     browserName: 'MicrosoftEdge',
-    //     platformName: 'Windows 10',
-    //     browserVersion: 'latest',
-    //     'sauce:options': {
-    //         ...defaultBrowserSauceOptions,
-    //     },
-    // },
-    // // Safari 11 is not W3C compliant,
-    // // see https://developer.apple.com/documentation/webkit/macos_webdriver_commands_for_safari_11_1_and_earlier
-    // {
-    //     browserName: 'safari',
-    //     platform: 'macOS 10.13',
-    //     version: '11.1',
-    //     ...defaultBrowserSauceOptions,
-    // },
-    // // Safari 13
-    // {
-    //     browserName: 'safari',
-    //     platformName: 'macOS 10.15',
-    //     browserVersion: 'latest',
-    //     'sauce:options': {
-    //         ...defaultBrowserSauceOptions,
-    //     },
-    // },
-    // // // Safari 12
-    // {
-    //     browserName: 'safari',
-    //     platformName: 'macOS 10.14',
-    //     browserVersion: 'latest',
-    //     'sauce:options': {
-    //         ...defaultBrowserSauceOptions,
-    //     },
-    // }
+    {
+        browserName: 'MicrosoftEdge',
+        platformName: 'Windows 10',
+        browserVersion: '18.17763',
+        'sauce:options': {
+            ...defaultBrowserSauceOptions,
+        },
+    },
+    {
+        browserName: 'MicrosoftEdge',
+        platformName: 'Windows 10',
+        browserVersion: 'latest',
+        'sauce:options': {
+            ...defaultBrowserSauceOptions,
+        },
+    },
+    // Safari 11 is not W3C compliant,
+    // see https://developer.apple.com/documentation/webkit/macos_webdriver_commands_for_safari_11_1_and_earlier
+    {
+        browserName: 'safari',
+        platform: 'macOS 10.13',
+        version: '11.1',
+        ...defaultBrowserSauceOptions,
+    },
+    // Safari 13
+    {
+        browserName: 'safari',
+        platformName: 'macOS 10.15',
+        browserVersion: 'latest',
+        'sauce:options': {
+            ...defaultBrowserSauceOptions,
+        },
+    },
+    // Safari 12
+    {
+        browserName: 'safari',
+        platformName: 'macOS 10.14',
+        browserVersion: 'latest',
+        'sauce:options': {
+            ...defaultBrowserSauceOptions,
+        },
+    }
 ];
 
 config.services = config.services.concat([
