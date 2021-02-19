60c5eb7ba5730aa1d4e560db7836247b62f4a994
Wim Selles <wim@wswebcreation.nl>
2021-02-19 08:35:14 +0100

    feat:new release

--- new/scripts/postbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Move to root folder
-cd ../
-
-# copy the content of the build folder to the main build folder
-cp -a ./build/. ../dist/new/
--- test/e2e/configs/wdio.shared.conf.js
+++ test/e2e/configs/wdio.shared.conf.js
@@ -1,33 +1,33 @@
 exports.config = {
-    // ====================
-    // Runner Configuration
-    // ====================
-    runner: 'local',
-    // ==================
-    // Specify Test Files
-    // ==================
-    specs: [
-        './test/e2e/specs/**/*.js'
-    ],
-    // ============
-    // Capabilities
-    // ============
-    maxInstances: 100,
-    // capabilities can be found in the `wdio.local.chrome.conf.js` or `wdio.sauce.conf.js`
-    // ===================
-    // Test Configurations
-    // ===================
-    logLevel: 'silent',
-    bail: 0,
-    baseUrl: 'http://localhost:3000',
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 90000,
-    connectionRetryCount: 3,
-    framework: 'jasmine',
-    reporters: ['spec'],
-    jasmineNodeOpts: {
-        defaultTimeoutInterval: 60000,
-        helpers: [require.resolve('@babel/register')],
-    },
-    services: []
+  // ====================
+  // Runner Configuration
+  // ====================
+  runner: 'local',
+  // ==================
+  // Specify Test Files
+  // ==================
+  specs: [
+    './test/e2e/specs/**/*.js'
+  ],
+  // ============
+  // Capabilities
+  // ============
+  maxInstances: 20,
+  // capabilities can be found in the `wdio.local.chrome.conf.js` or `wdio.sauce.conf.js`
+  // ===================
+  // Test Configurations
+  // ===================
+  logLevel: 'silent',
+  bail: 0,
+  baseUrl: 'http://localhost:3000',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+  framework: 'jasmine',
+  reporters: ['spec'],
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 60000,
+    helpers: [require.resolve('@babel/register')],
+  },
+  services: [],
 };
