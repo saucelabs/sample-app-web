30e16eaabe7f86c1a227e9fd9818bfdcf0015b52
Wim Selles <wim@wswebcreation.nl>
2021-02-22 19:42:43 +0100

    feat: add button to stories

--- src/storybook/stories/Button.stories.js
+++ src/storybook/stories/Button.stories.js
@@ -1,36 +1,67 @@
-// import React from "react";
-//
-// import { Button } from "./Button";
-//
-// export default {
-//   title: "Example/Button",
-//   component: Button,
-//   argTypes: {
-//     backgroundColor: { control: "color" },
-//   },
-// };
-//
-// const Template = (args) => <Button {...args} />;
-//
-// export const Primary = Template.bind({});
-// Primary.args = {
-//   primary: true,
-//   label: "Button",
-// };
-//
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//   label: "Button",
-// };
-//
-// export const Large = Template.bind({});
-// Large.args = {
-//   size: "large",
-//   label: "Button",
-// };
-//
-// export const Small = Template.bind({});
-// Small.args = {
-//   size: "small",
-//   label: "Button",
-// };
+import React, { useState } from "react";
+
+import Button, { BUTTON_SIZES, BUTTON_TYPES } from "../../components/Button";
+
+export default {
+  title: "SwagLabs/Button",
+  component: Button,
+};
+
+const Template = (args) => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <>
+      <Button {...args} onClick={() => setValue(value + 1)} />
+      <p>
+        You clicked <strong>{value}</strong> times.
+      </p>
+    </>
+  );
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: "Primary",
+  size: BUTTON_SIZES.MEDIUM,
+  type: BUTTON_TYPES.PRIMARY,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: "Secondary",
+  size: BUTTON_SIZES.MEDIUM,
+  type: BUTTON_TYPES.SECONDARY,
+};
+
+export const Action = Template.bind({});
+Action.args = {
+  label: "Some Action",
+  size: BUTTON_SIZES.MEDIUM,
+  type: BUTTON_TYPES.ACTION,
+};
+
+export const Back = Template.bind({});
+Back.args = {
+  label: "Go back",
+  size: BUTTON_SIZES.MEDIUM,
+  type: BUTTON_TYPES.BACK,
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  label: "Small",
+  size: BUTTON_SIZES.SMALL,
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  label: "Medium button",
+  size: BUTTON_SIZES.MEDIUM,
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  label: "Large button",
+  size: BUTTON_SIZES.LARGE,
+};
