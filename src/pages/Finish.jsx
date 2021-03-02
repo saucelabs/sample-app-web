ee2ed3bd06be79b1402ed144ad12abec085618e8
Wim Selles <wim@wswebcreation.nl>
2021-03-02 12:28:42 +0100

    feat: update checkout complete layout (#56)
    - fix layout
    - add UTs

--- src/pages/Finish.jsx
+++ src/pages/Finish.jsx
@@ -3,9 +3,12 @@ import { withRouter } from "react-router-dom";
 import PonyExpress from "../assets/img/pony-express.png";
 import SwagLabsFooter from "../components/Footer";
 import HeaderContainer from "../components/HeaderContainer";
+import PropTypes from "prop-types";
+import Button, { BUTTON_SIZES } from "../components/Button";
+import { ROUTES } from "../utils/Constants";
 import "./Finish.css";
 
-function Finish() {
+const Finish = ({ history }) => {
   return (
     <div id="page_wrapper" className="page_wrapper">
       <div id="contents_wrapper">
@@ -20,11 +23,24 @@ function Finish() {
             pony can get there!
           </div>
           <img alt="Pony Express" className="pony_express" src={PonyExpress} />
+          <Button
+            label="Back Home"
+            onClick={() => history.push(ROUTES.INVENTORY)}
+            size={BUTTON_SIZES.SMALL}
+          />
         </div>
       </div>
       <SwagLabsFooter />
     </div>
   );
-}
+};
+Finish.propTypes = {
+  /**
+   * The history
+   */
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default withRouter(Finish);
