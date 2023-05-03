b3e36ec1cc1e5f91bb8efba030d0d39ea3ba893e
miked <mikedonovan1987@gmail.com>
2023-05-03 02:03:45 -0400

    separating the server and test into 2 containers (#94)

--- orchestrate.sh
+++ orchestrate.sh
@@ -2,6 +2,4 @@
 
 sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY --region us-west --tunnel-name mdonovan2010_tunnel_name &> sc-orchestrate.log &
 
-npm run start &> server-orchestrate.log &
-
-npm run test.e2e.sauce.us-orchestrate
+npm run start
