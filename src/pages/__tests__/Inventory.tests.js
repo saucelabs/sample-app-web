2bf78294911ddf05070b5e38e79061eda5092bfe
paweltomaszewskisaucelabs <119342898+paweltomaszewskisaucelabs@users.noreply.github.com>
2024-03-26 11:28:58 +0100

    IRIS-826 add long inventory list (#115)
    * IRIS-826 add long inventory list
    
    * add missing prop
    
    * add missing prop
    
    * update test
    
    * fix test

--- src/pages/__tests__/Inventory.tests.js
+++ src/pages/__tests__/Inventory.tests.js
@@ -2,6 +2,8 @@ import React, { useState as useStateMock } from "react";
 import { shallow } from "enzyme";
 import Inventory from "../Inventory";
 import * as Credentials from "../../utils/Credentials";
+import {InventoryData} from "../../utils/InventoryData";
+import {InventoryDataLong} from "../../utils/InventoryDataLong";
 
 jest.mock("react", () => ({
   ...jest.requireActual("react"),
@@ -20,14 +22,19 @@ describe("Inventory", () => {
   });
 
   it("should render correctly", () => {
-    const wrapper = shallow(<Inventory.WrappedComponent />);
+    const wrapper = shallow(<Inventory.WrappedComponent data={InventoryData} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render correctly long", () => {
+    const wrapper = shallow(<Inventory.WrappedComponent data={InventoryDataLong} />);
+    expect(wrapper.find('[data-test="inventory-list"]').length).toEqual(1);
   });
 
   it("should render correctly for a problem user", () => {
     const isProblemUserSpy = jest.spyOn(Credentials, "isProblemUser");
     isProblemUserSpy.mockReturnValue(true);
-    const wrapper = shallow(<Inventory.WrappedComponent />);
+    const wrapper = shallow(<Inventory.WrappedComponent data={InventoryData} />);
 
     expect(wrapper).toMatchSnapshot();
     isProblemUserSpy.mockClear();
@@ -36,7 +43,7 @@ describe("Inventory", () => {
   it("should render correctly for a visual user", () => {
     const isVisualUserSpy = jest.spyOn(Credentials, "isVisualUser");
     isVisualUserSpy.mockReturnValue(true);
-    const wrapper = shallow(<Inventory.WrappedComponent />);
+    const wrapper = shallow(<Inventory.WrappedComponent data={InventoryData} />);
 
     expect(wrapper).toMatchSnapshot();
     isVisualUserSpy.mockClear();
